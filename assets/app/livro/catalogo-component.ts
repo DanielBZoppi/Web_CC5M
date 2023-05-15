1896f80bbda79b35ce79d6cbf9943f2601a977ac
DanielBZoppi <danzoppi@hotmail.com>
2023-05-14 22:42:00 -0300

    atual

--- assets/app/livro/catalogo-component.ts
+++ assets/app/livro/catalogo-component.ts
@@ -5,7 +5,14 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-catalogo',
-    templateUrl: './catalogo-component.html',
+    templateUrl: './catalogo-component.html', 
+    template: `
+    <div class="col-md8 col-md-offset-2">
+            <app-catalogo [messageVarClasse]=""
+            (editClicked_MessageMetodoClasse)="msg.content = $event"
+            *ngFor="let msg of messageS">
+        </app-catalogo>
+    </div>`,
     styleUrls:["catalogo-component.css"]
 })
 
