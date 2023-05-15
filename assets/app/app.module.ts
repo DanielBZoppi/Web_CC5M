aeb8ddb1a85461466747ae012eb9b4bdf6a582e7
DanielBZoppi <danzoppi@hotmail.com>
2023-05-14 23:38:46 -0300

    atual

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from "@angular/http";
-
 import { AppComponent } from "./app.component";
 import { MessageListComponent } from './messages/message-list.component';
 import { MessageComponent } from './messages/message.component';
--- assets/app/livro/catalogo-component.ts
+++ assets/app/livro/catalogo-component.ts
@@ -6,14 +6,14 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 @Component({
     selector: 'app-catalogo',
     templateUrl: './catalogo-component.html', 
-    template: `
-    <div class="col-md8 col-md-offset-2">
-            <app-catalogo [messageVarClasse]=""
-            (editClicked_MessageMetodoClasse)="msg.content = $event"
-            *ngFor="let msg of messageS">
-        </app-catalogo>
-    </div>`,
-    styleUrls:["catalogo-component.css"]
+    // template: `
+    // <div class="col-md8 col-md-offset-2">
+    //         <app-catalogo [messageVarClasse]="imagePath"
+    //         (editClicked_MessageMetodoClasse)="imagePath.content = $event"
+    //         *ngFor="let msg of messageS">
+    //     </app-catalogo>
+    // </div>`,
+     styleUrls:["catalogo-component.css"]
 })
 
 export class CatalogoComponent {
